45c88073b5ef1eb1b5daf16b9d08009d069ab38f
Mukesh Pilaniya <mukesh.pilaniya@cyware.com>
2022-04-24 23:43:25 +0530

    Created routes forgenerating and fetching shorter urls

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,1 +1,20 @@
 package api
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"net/http"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// Common middleware for all routes
+	mux.Use(middleware.Logger)
+
+	// public routes
+	mux.Post("/api/v1/generate_url", app.generateUrl)
+	mux.Post("/api/v1/fetch_url", app.fetchUrl)
+
+	return mux
+}
